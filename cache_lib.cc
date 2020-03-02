d9719547d039f55a223c12c92266bef8d5e9be1b
Hien Nguyen <nguyehi@reed.edu>
2020-03-02 15:34:04 -0800

    resolve variable scope

--- cache_lib.cc
+++ cache_lib.cc
@@ -1,34 +1,63 @@
+#include <unordered_map>
+
 #include "cache.hh"
 #include "evictor.hh"
 
 using namespace std;
 
-
-class Impl {
-
-  size_type maxmem;
-  float max_load_factor;
-  Evictor* evictor;
-  hash_func hasher;
-
-  Impl(size_type maxmem,
-      float max_load_factor,
-      Evictor* evictor,
-      hash_func hasher) : maxmem(maxmem), max_load_factor(max_load_factor), evictor(evictor), hasher(hasher)
-      {}
-
-  ~Impl() = default;
-
-  void set(key_type key, val_type val, size_type size) {}
-
-  val_type get(key_type key, size_type& val_size) const;
-
-  bool del(key_type key);
-
-  size_type space_used() const;
-
-  void reset();
+/*
+// Implement Universal Hashing: https://en.wikipedia.org/wiki/Universal_hashing
+size_type std::hash<key_type>(key_type key, size_type a, size_type p)
+{
+  size_type hash = 0;
+  for (size_type i=0; i< key.length(); ++i) {
+    hash = ((h*a) + key[i]) % p;
+  }
+  return hash;
 }
+*/
+
+class Cache::Impl {
+  public:
+    size_type maxmem;
+    float max_load_factor;
+    Evictor* evictor;
+    hash_func hasher;
+
+    unordered_map<key_type, val_type, hash_func> m_cache;
+    byte_type memused;
+
+    Impl(size_type maxmem,
+        float max_load_factor,
+        Evictor* evictor,
+        hash_func hasher) : maxmem(maxmem), max_load_factor(max_load_factor), evictor(evictor), hasher(hasher),
+                            memused(0), m_cache(0, hasher)
+        {}
+
+    ~Impl() = default;
+
+    void set(key_type key, val_type val, size_type size)
+    {
+      if (memused + size > maxmem) {
+        while (memused + size > maxmem) {
+          auto item = m_cache.begin();
+          del(item->first);
+          memused -= sizeof(item->second);
+        }
+      }
+      m_cache[key] = val;
+      memused += size;
+      return;
+    }
+
+    val_type get(key_type key, size_type& val_size) const {};
+
+    bool del(key_type key) {};
+
+    size_type space_used() const {};
+
+    void reset() {};
+};
 
 Cache::Cache(size_type maxmem,
       float max_load_factor,
