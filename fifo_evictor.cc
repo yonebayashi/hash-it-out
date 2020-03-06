872687f9463e082c0f9ba41e8479fd00d22378d5
david tamas-parris <davidtp@reed.edu>
2020-03-05 17:46:20 -0800

    changed fifo_evictor underlying container

--- fifo_evictor.cc
+++ fifo_evictor.cc
@@ -4,15 +4,20 @@
 #include "fifo_evictor.hh"
 
 
-FifoEvictor::FifoEvictor(std::queue<key_type>& key_list) : key_list(key_list) {}
+FifoEvictor::FifoEvictor(std::queue<key_type>& key_queue) : key_queue(key_queue) {}
 
-void FifoEvictor::touch_key(const key_type&) {}
+void FifoEvictor::touch_key(const key_type& key) {
+	key_queue.push(key);
+	return;
+}
 
 const key_type FifoEvictor::evict()  {
-  if (key_list.empty()) {
+  if (key_queue.empty()) {
     return "";
   }
-  key_type evicted = key_list.front();
-  key_list.pop();
+  key_type evicted = key_queue.front();
+  key_queue.pop();
   return evicted->first;
 }
+
+
--- fifo_evictor.hh
+++ fifo_evictor.hh
@@ -3,19 +3,18 @@
 #include "evictor.hh"
 
 
-class FifoEvictor : class Evictor {
+class FifoEvictor : public Evictor {
 private:
-  std::queue<key_type> key_list;
+  std::queue<key_type> key_queue;
 
 public:
-  FifoEvictor(std::queue<key_type>& key_list);
+  FifoEvictor(std::queue<key_type>& key_queue);
   ~FifoEvictor() = default;
 
   // Disallow copies
   FifoEvictor(const FifoEvictor&) = delete;
   FifoEvictor& operator=(const FifoEvictor&) = delete;
 
-  void touch_key(const key_type&);
+  void touch_key(const key_type& key); //Is it kosher to pass an argument here?
   const key_type evict();
-  };
-}
+};
