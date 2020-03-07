261ea8868e959842bfa68739e95011081be39b68
david tamas-parris <davidtp@reed.edu>
2020-03-07 15:33:49 -0800

    added tests, more to come

--- test_cache_lib.cc
+++ test_cache_lib.cc
@@ -9,11 +9,14 @@ using namespace std;
 
 
 void testCacheBasic() {
-  Cache basic_cache(8);
+  FifoEvictor* evictor= new FifoEvictor();
+  Cache basic_cache(8, 0.75, evictor);
 
   const Cache::val_type val1 = "1";
   const Cache::val_type val2 = "2";
   const Cache::val_type val3 = "16";
+  const Cache::val_type val4 = "17";
+  const Cache::val_type val5 = "18";
 
   assert(basic_cache.space_used() == 0);  // cache is initialliy empty
   cout << "Test empty cache passed" << endl;
@@ -44,7 +47,50 @@ void testCacheBasic() {
   cout << "Retrieving key 1... " << basic_cache.get("k1", size) << endl;
 
   assert(basic_cache.get("k1", size) == "1");
+
   cout << "Test get key 1 passed" << endl;
+
+  cout << "Attempting to retreive key 4 (not yet in cache)... " << '"'<<basic_cache.get("k4", size) <<'"'<< endl;
+
+  assert(basic_cache.get("k4", size) == "");
+
+  cout << "Test for retreiving unentered key passed" << endl;
+  //Change a key already in cache
+  cout << "Inserting val4  of size " << strlen(val3)+1 << " bytes at key 3" endl;
+  basic_cache.set("k3", val4, strlen(val4)+1);
+  assert(basic_cache.space_used() == 7);
+
+  //Show the value changed
+  cout << "Retrieving key 3... " << basic_cache.get("k3", size) << endl;
+
+  assert(basic_cache.get("k3", size) == "17");
+
+  cout << "Test get key 1 passed" << endl;
+
+  //TODO: show the memory at old address is now deleted?
+
+  // Does this do it? cout<< "Viewing memory at val4... "<< '"'<<val4<<'"'<<endl;
+  // Corresponding assertion? : assert(val4=="");
+
+
+  //Testing Deletion and querying after deletion: 
+  cout<<"Deleting key 4... "<<endl;
+  basic_cache.del("k4");
+  cout << "Attempting to retreive key 4 (deleted from cache)... " << '"'<<basic_cache.get("k4", size) <<'"'<< endl;
+  assert(basic_cache.get("k4", size) == "");
+  cout <<"Test passed for querying after deletion"<<endl;
+
+
+  //TODO: 
+  //Testing eviction after memory overflow AND
+  //querying for both evicted value and newly added value:
+
+
+
+
+
+
+
 }
 
 int main(int argc, char const *argv[]) {
